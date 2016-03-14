1b362f599c970f13e62371ecab5810d320a971e3
koukumar <kousik.kumar@gmail.com>
2016-03-14 15:37:22 +0530

    added secret key check for user api

--- routes/user.js
+++ routes/user.js
@@ -6,6 +6,8 @@ var loggly = require('loggly');
 var loggly_token = process.env.LOGGLY_TOKEN;
 var loggly_sub_domain = process.env.LOGGLY_SUB_DOMAIN;
 
+var secret = process.env.SECRET_KEY;
+
 var client = loggly.createClient({
     token: loggly_token,
     subdomain: loggly_sub_domain,
@@ -14,6 +16,11 @@ var client = loggly.createClient({
 });
 
 router.get('/:account/:email', function(req, res) {
+    if (req.headers['secret'] != secret) {
+        res.status(400).send("Bad credentials");
+        return;
+    }
+
     var account = req.params.account;
     var email = req.params.email;
     mail.newUserNotification(account, email);
